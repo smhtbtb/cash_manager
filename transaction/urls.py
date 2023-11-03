03ded661fdc3161fe5bdb59b3dc50e610608771e
Seyyed Mohammad Hosein Tabatabaei <83072931+smhtbtb@users.noreply.github.com>
2023-11-04 00:29:52 +0330

    Balance Tracking

--- transaction/urls.py
+++ transaction/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import TransactionList, TransactionDetail
+from .views import TransactionList, TransactionDetail, BalanceView
 
 urlpatterns = [
     path('', TransactionList.as_view(), name='transaction-list'),
     path('<int:pk>', TransactionDetail.as_view(), name='transaction-detail'),
+    path('get-balance', BalanceView.as_view(), name='get-balance'),
 ]
--- transaction/views.py
+++ transaction/views.py
@@ -1,5 +1,9 @@
+from django.db import models
 from rest_framework import generics, filters
+from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from .models import Transaction
 from .serializers import TransactionSerializer
@@ -18,3 +22,21 @@ class TransactionDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Transaction.objects.all()
     serializer_class = TransactionSerializer
     permission_classes = [IsAuthenticated]
+
+
+class BalanceView(APIView):
+    def get(self, request):
+        calculate_balance = self.calculate_balance()
+        return Response(calculate_balance, status=status.HTTP_200_OK)
+
+    def calculate_balance(self):
+        # Calculate the cumulative balance based on existing transactions
+        previous_transactions = Transaction.objects.all()
+        cumulative_balance = previous_transactions.filter(type='income').aggregate(
+            total_income=models.Sum('amount')
+        )['total_income'] or 0
+        cumulative_balance -= previous_transactions.filter(type='expense').aggregate(
+            total_expense=models.Sum('amount')
+        )['total_expense'] or 0
+        return {"cumulative_balance": float(cumulative_balance)}
+
